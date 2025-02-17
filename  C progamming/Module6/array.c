b2fd6d3f7f1e22b880c44f9a06e6d5840c66a4d5
user <user@Soumen.local>
2025-02-17 22:11:52 +0600

    Module 4

---
+++  C progamming/Module6/array.c
@@ -0,0 +1,6 @@
+#include<stdio.h>
+int main(){
+
+    int shakib[100000];
+    return 0;
+}
---
+++  C progamming/Module6/arrayInitialization.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+
+    int mou[5];
+    for(int i=0;i<=5;i++){
+        printf("mou[%d]\n",i);
+    }
+    return 0;
+}
